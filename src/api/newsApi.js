9881c2e5d23e98a10ec6480e7a12e425d0993fbc
Devonta Harris <devontah94@gmail.com>
2022-03-09 17:30:05 -0500

    cors fix attempt

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://newsapi.org/v2/',
     headers: {
-        'X-Api-Key': 'd020f53f5de3456eb05fbe6eee0e5ff3'
+        'X-Api-Key': 'd020f53f5de3456eb05fbe6eee0e5ff3',
+        'Access-Control-Allow-Origin': '*'
     }
 })
